b92ca5332e6e46d7db0d263d11acac9ba826b3f9
RChilali <chilali.lmc@gmail.com>
2023-04-15 21:55:11 +0200

    intellij compilator

--- Mario_Java/src/main/java/org/example/Main.java
+++ Mario_Java/src/main/java/org/example/Main.java
@@ -1,7 +1,23 @@
 package org.example;
 
+import javax.swing.*;
+
 public class Main {
+
+    public static Scene scene;
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // creation de la fenetre de l'application
+        JFrame window = new JFrame("Mario_Project");
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setSize(800, 600);
+        window.setLocationRelativeTo(null);
+        window.setResizable(false);
+        window.setAlwaysOnTop(true);
+
+        scene = new Scene();
+
+        window.setContentPane(scene);
+        window.setVisible(true);
+
     }
 }
